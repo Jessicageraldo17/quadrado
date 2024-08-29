cadfbf23e736e2a01552c354658538a8c142e0a8
Jessicageraldo17 <jessica.geraldo.barbosa@escola.pr.gov.br>
2024-08-29 11:28:04 -0300

    Update and rename README.md to sketch.js

---
+++ sketch.js
@@ -0,0 +1,10 @@
+# quadradofunction setup() {
+  createCanvas(400, 400);background("rgb(233,7,169)");
+}
+
+function draw() {
+
+if(mouseIsPressed) rect(mouseX,mouseY,20,20)
+  stroke("rgb(241,255,0)");
+ fill("#15CCBD");
+}
